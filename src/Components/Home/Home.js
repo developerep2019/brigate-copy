10dcf46e4a213ab3a058f7e8f87b8ac02aa62fc9
developerep2019 <developerep2019@gmail.com>
2020-12-04 20:41:26 +0600

    started working

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../Header/Header.js';
+const Home = () => {
+    return (
+        <div>
+            <Header/>
+        </div>
+    );
+};
+
+export default Home;
